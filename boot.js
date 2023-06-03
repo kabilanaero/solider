68f643fa0f2b40ee083ab7e875bd1960873b6e83
kabilanaero <kabilan.thangadurai7@gmail.com>
2023-06-03 12:50:23 +0530

    bootstrap

---
+++ boot.js
@@ -0,0 +1,20 @@
+$('.client').owlCarousel({
+    loop:true,
+	start:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
